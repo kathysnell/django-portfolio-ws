d606b816a24d30bd70e79a51a64831efaabd4ebc
kathysnell <77510257+kathysnell@users.noreply.github.com>
2026-04-21 16:15:34 -0400

    Test implementation.

--- body/tests.py
+++ body/tests.py
@@ -1,3 +1,90 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.db import models
+from .models import Card, CardSide, BodyContent
+from .views import BodyListView, body_detail, dynamic_page_router
+from django.urls import reverse
+from http import HTTPStatus
+from core.views import tinymce_upload_image
 
-# Create your tests here.
+### --- Card, CardSide models testing
+class CardTest(TestCase):
+    def test_model_properties(self):
+        card = Card.objects.create(box=1, active=True)
+        front = CardSide.objects.create(card=card, is_front=True)
+        back = CardSide.objects.create(card=card, is_front=False)
+           
+        self.assertEqual(card.box, 1)
+        self.assertIsInstance(front, CardSide, "front should be of type CardSide")
+        self.assertIsInstance(back, CardSide, "back should be of type CardSide" )
+        self.assertEqual(front.is_front, True)
+        self.assertEqual(back.is_front, False)
+        self.assertEqual(card.active, True)
+    
+    def test_foreign_key_definition(self):
+        field = CardSide._meta.get_field('card')
+        
+        self.assertIsInstance(field, models.ForeignKey)
+        self.assertEqual(field.related_model, Card)
+
+### --- BodyContent model testing  
+class TestBodyContent(TestCase):
+    def test_model_properties(self):
+        body = BodyContent.objects.create(bgcolor="#123456", active=True)
+
+        self.assertEqual(body.bgcolor, "#123456")
+        self.assertEqual(body.active, True)
+
+### --- BodyViews testing
+class TestBodyViews(TestCase):
+    def test_get_default_list(self):
+        # Create some Card and BodyContent instances
+        card1 = Card.objects.create(box=1, active=True)
+        card2 = Card.objects.create(box=1, active=True, page='test')
+        body1 = BodyContent.objects.create(active=True)
+        body2 = BodyContent.objects.create(active=True, page='test')
+
+        from .views import get_default_list
+
+        # Test get_default_list with Card queryset
+        card_queryset = get_default_list(Card.objects.all())
+        self.assertIn(card1, card_queryset)
+        self.assertNotIn(card2, card_queryset)
+
+        # Test get_default_list with BodyContent queryset
+        body_queryset = get_default_list(BodyContent.objects.all())
+        self.assertIn(body1, body_queryset)
+        self.assertNotIn(body2, body_queryset)
+
+    ### --- body_detail view testing
+    def test_body_detail_view(self):
+        # Create a Card and BodyContent with specific page
+        card = Card.objects.create(box=1, active=True, page='test')
+        body = BodyContent.objects.create(active=True, page='test')
+
+        # Create a request to the body_detail view
+        factory = RequestFactory()
+        request = factory.get('/test/')
+
+        # Call the view
+        response = body_detail(request)
+
+        # Check that the response is 200 OK
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+    ### --- dynamic_page_router view testing
+    def test_dynamic_page_router(self):
+        # Create a BodyContent with specific page
+        body = BodyContent.objects.create(active=True, page='test')
+
+        # Create a request to the dynamic_page_router view
+        factory = RequestFactory()
+        request = factory.get('/test/')
+
+        # Call the view
+        response = dynamic_page_router(request, slug='test')
+
+        # Check that the response is 200 OK
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+  
--- intro/tests.py
+++ intro/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth import authenticate, login
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.urls import reverse
+from django.http import HttpRequest
 
-# Create your tests here.
+from intro.models import Intro
+
+### --- Intro model testing
+class TestIntro(TestCase):
+    def test_model_properties(self):
+        intro = Intro.objects.create(bgcolor="#123456", active=True)
+
+        self.assertEqual(intro.bgcolor, "#123456")
+        self.assertEqual(intro.active, True)
+
+### --- Tinymce image upload testing
+class TestTinymceUploadImage(TestCase):
+    def setUp(self):
+        request = HttpRequest()
+        user = authenticate(request, username='admin', password='password')
+        if user:
+            # Manually attach the user to the client session
+            self.client.force_login(user)
+
+    def test_image_upload_as_superuser(self):
+        image_content = b'GIF89a\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00!\xf9\x04\x01\x00\x00\x00\x00,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;'
+        test_image = SimpleUploadedFile('test.gif', image_content, content_type='image/gif')
+
+        response = self.client.post(reverse('tinymce_upload_image'), {'image': test_image})
+        self.assertEqual(response.status_code, 301) # Expect redirect on success (HTTP->HTTPS)
+     

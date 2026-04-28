ba2fc3c978326f61e637ccfb0f1277ae7394f601
kathysnell <77510257+kathysnell@users.noreply.github.com>
2026-04-28 13:22:20 -0400

    Test update.

--- intro/tests.py
+++ intro/tests.py
@@ -27,6 +27,6 @@ class TestTinymceUploadImage(TestCase):
         image_content = b'GIF89a\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00!\xf9\x04\x01\x00\x00\x00\x00,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;'
         test_image = SimpleUploadedFile('test.gif', image_content, content_type='image/gif')
 
-        response = self.client.post(reverse('tinymce_upload_image'), {'image': test_image})
-        self.assertEqual(response.status_code, 301) # Expect redirect on success (HTTP->HTTPS)
+        response = self.client.post(reverse('tinymce_upload_image'), {'storage_path': test_image})
+        self.assertEqual(response.status_code, 302) # Expect redirect on success
      
